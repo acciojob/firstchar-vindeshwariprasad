854bf7d93c89f4f26a490543c442676eedf7467b
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 04:49:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function firstChar(text) {
+function firstChar(str) {
   // your code here
+	str = str.trim();
+    
+    // Check if the trimmed string is empty
+    if (str === '') {
+        return '';
+    }
+    
+    // Iterate through each character of the trimmed string
+    for (let char of str) {
+        // If the character is not a space, return it
+        if (char !== ' ') {
+            return char;
+        }
+    }
+    
+    // If all characters are spaces, return an empty string
+    return '';
 }
 
 // Do not change the code below
